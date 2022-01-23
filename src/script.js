99e516890fc967e93f777b4c0044944fa30791c4
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-01-23 20:47:52 +0700

    .

--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
 const textIn = document.getElementsByName("textin")[0];
+const textOut = document.getElementsByName("textout")[0];
+const selvalue = document.getElementById("selector");
+
+
+
+
 
 textIn.onchange(
     ()=>update(textIn.value)
